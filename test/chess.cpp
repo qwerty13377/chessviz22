77f28d5b93504f8013123bdb3be1fdc8f3555fd8
Vadim Efremov <ottofonsmorc@yandex.ru>
2019-05-29 08:41:44 +0700

    Fix chess

--- test/chess.cpp
+++ test/chess.cpp
@@ -1,56 +1,60 @@
-#include "../src/chess1.h"
+#include "../src/chess.h"
+#define CATCH_CONFIG_MAIN
+#include "../thirdparty/catch/catch.hpp"
 
-//#include "../thirdparty/ctest.h"
-
-// CTEST(input_step, step_1)
-int test1()
+TEST_CASE("Test1")
 {
-    // Given
-    char pole[9][9] = {{'8', 'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
-                       {'7', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
-                       {'6', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                       {'5', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                       {'4', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                       {'3', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                       {'2', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-                       {'1', 'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
-                       {' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'}};
-
-    const int side = 0;
-    const int i2 = 6;
-    const int j2 = 1;
-
-    // When
-    const int result = FriendlyFire(side, i2, j2, pole);
-
-    // Then
-    const int expected = 0;
-    return expected == result;
-    //    ASSERT_EQUAL(expected, result);
+    REQUIRE(1 == 1);
 }
 
-int test2()
+char pole[9][9] = {{'8', 'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
+                   {'7', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+                   {'6', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                   {'5', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                   {'4', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                   {'3', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                   {'2', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+                   {'1', 'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
+                   {' ', 'a', '6', 'c', 'd', 'e', 'f', 'g', 'h'}};
+
+TEST_CASE("Test2") // move p
 {
-    // Given
-    char pole[9][9] = {{'8', 'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
-                       {'7', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
-                       {'6', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                       {'5', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                       {'4', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                       {'3', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                       {'2', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-                       {'1', 'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
-                       {' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'}};
-
-    const int side = 1;
-    const int i2 = 1;
-    const int j2 = 1;
-
-    // When
-    const int result = FriendlyFire(side, i2, j2, pole);
-
-    // Then
-    const int expected = 0;
-    return expected == result;
-    //    ASSERT_EQUAL(expected, result);
+    int i1 = 6, j1 = 5, i2 = 5, j2 = 5;
+    int result = MoveCheck(0, i1, i2, j1, j2, pole);
+    int expected = 1;
+    REQUIRE(expected == result);
+}
+TEST_CASE("Test3") // atack p
+{
+    int i1 = 6, j1 = 4, i2 = 5, j2 = 5;
+    pole[5][5] = 'r';
+    int result = MoveCheck(0, i1, i2, j1, j2, pole);
+    int expected = 1;
+    pole[5][5] = ' ';
+    REQUIRE(expected == result);
+}
+TEST_CASE("Test4") // hourse left
+{
+    int i1 = 7, j1 = 2, i2 = 5, j2 = 3;
+    int result = MoveCheck(0, i1, i2, j1, j2, pole);
+    int expected = 1;
+    REQUIRE(expected == result);
+}
+TEST_CASE("Test5") // elephant left
+{
+    pole[6][4] = ' ';
+    int i1 = 7, j1 = 3, i2 = 5, j2 = 5;
+    int result = MoveCheck(0, i1, i2, j1, j2, pole);
+    int expected = 1;
+    pole[6][4] = 'P';
+    REQUIRE(expected == result);
+}
+TEST_CASE("Test6") // king white
+{
+    pole[6][5] = ' ';
+    int i1 = 7, j1 = 5, i2 = 6, j2 = 5;
+    int result = MoveCheck(0, i1, i2, j1, j2, pole);
+    int expected = 1;
+    pole[6][5] = 'P';
+    REQUIRE(expected == result);
 }
